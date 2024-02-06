a8004f9f40ff1fa594c50d6480ecdc90e2b74652
HPrzybysz <niewica@gmail.com>
2024-02-06 23:55:47 +0100

    Projects done so far

---
+++ 3_interfejsy/Program.cs
@@ -0,0 +1,91 @@
+﻿using _3_interfejsy.classes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_interfejsy
+{
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      List<Book> books = new List<Book>();
+
+      books.Add(new Book("Hobbit", "Nowak", 1937, 45.99));
+      books.Add(new Book("Hobbit2", "Pawlak", 2000, 145.99));
+      books.Add(new Book("Hobbit3", "Arbuz", 2000, 5.99));
+      books.Add(new Book("Hobbit4", "Arbuz", 1948, 5.99));
+
+      Console.WriteLine("Lista książek:");
+      foreach (Book book in books)
+      {
+        Console.WriteLine(book);
+      }
+
+      books.Sort();
+      Console.WriteLine("\nPosortowanie książki według ceny:");
+      foreach (Book book in books)
+      {
+        Console.WriteLine(book);
+      }
+
+      Console.WriteLine("\nSortowanie książek według daty publikacji:");
+      //var sortedByYear = books.OrderBy(e => e.YearOfPublication);
+      var sortedByYear = books.OrderBy(book => book.YearOfPublication);
+      foreach (Book book in sortedByYear)
+      {
+        Console.WriteLine(book);
+      }
+
+      Console.WriteLine("\nSortowanie książek według daty publikacji od najmłodszej do najstarszej:");
+      var sortedByDescYear = books.OrderByDescending(book => book.YearOfPublication);
+      foreach (Book book in sortedByDescYear)
+      {
+        Console.WriteLine(book);
+      }
+
+      Console.WriteLine("\nSortowanie książek według autora:");
+      var sortedByAuthor = books.OrderBy(book => book.Author);
+      foreach (Book book in sortedByAuthor)
+      {
+        Console.WriteLine(book);
+      }
+
+      Console.WriteLine("\nSortowanie książek według ceny nierosnąco, a następnie według roku publikacji od najstarszej książki:");
+
+      var sortedByPriceAndYear = books.OrderByDescending(book => book.Price).ThenBy(book => book.YearOfPublication);
+      foreach (Book book in sortedByPriceAndYear)
+      {
+        Console.WriteLine(book);
+      }
+
+      Console.ReadKey();
+    }
+    /* Interfejsy
+ Zdefiniuj klasę Book, która ma reprezentować informacje o książce, takie jak tytuł,
+autor, rok wydania i cena.
+ Zaimplementuj interfejs IComparable<Book> w klasie Book, który pozwala na
+porównywanie obiektów typu Book według ich ceny.Aby to zrobić, zdefiniuj metodę
+CompareTo(Book other), która zwraca liczbę całkowitą oznaczającą relację pomiędzy
+bieżącym obiektem a innym obiektem typu Book.
+ Zdefiniuj konstruktor klasy Book, który przyjmuje jako parametry tytuł, autora, rok
+wydania i cenę książki i inicjalizuje odpowiednie pola klasy.
+ Zdefiniuj metodę ToString() klasy Book, która zwraca reprezentację tekstową obiektu
+typu Book, składającą się z tytułu, autora, roku wydania i ceny książki, oddzielonych
+przecinkami.
+ Zdefiniuj klasę Program, która zawiera metodę Main, w której utwórz listę książek
+typu Book i dodaj do niej kilka przykładowych obiektów.
+ Posortuj listę książek według ceny, używając metody Sort klasy List<T>.Wyświetl
+posortowaną listę na konsoli, używając metody ToString() klasy Book.
+ Posortuj listę książek według innych kryteriów, np.daty publikacji, autora, tytułu,
+używając metod OrderBy, OrderByDescending i ThenBy z przestrzeni nazw
+System.Linq.Wyświetl posortowane listy na konsoli, używając metody ToString() klasy
+Book.
+*/
+
+  }
+}
---
+++ kartkowka25_01/Program.cs
@@ -0,0 +1,124 @@
+﻿namespace KasperskiFilip_gr1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zadanie 4
+            Console.WriteLine("\nZadanie 4");
+            List<int> randomInts = UtworzListe(10);
+            Random random = new Random();
+            for (int i = 0; i < randomInts.Count; i++)
+            {
+                randomInts[i] = random.Next(1, 101);
+            }
+            for (int i = 0; i < randomInts.Count; i++)
+            {
+                Console.WriteLine($"Wartość {i + 1} listy : {randomInts[i]}");
+            }
+
+            //zadanie 5
+            Console.WriteLine("\nZadanie 5");
+            int[,] tablica = UtworzTablice([1, 2, 3], [4,5,6], [7,8,9]);
+            int suma = 0;
+            for(int i=0; i<3; i++)
+            {
+                suma += tablica[i,i];
+            }
+            Console.WriteLine($"Suma liczba na przekątej tablicy wynosi: {suma}");
+
+            //zadanie 6
+            Console.WriteLine("\nZadanie 6");
+            Dictionary<string,string> slownik = new Dictionary<string, string>();
+            slownik.Add("kot", "miau");
+            slownik.Add("pies", "hau");
+            slownik.Add("krowa", "muuu");
+            
+            Dictionary<string, string> zwierzeta = UtworzSlownik([slownik]);
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Podaj nazwe zwierzęcia: ");
+                    string? input = Console.ReadLine();
+                    if (input == "koniec")
+                    {
+                        break;
+                    }
+                    if (slownik[input] != null)
+                    {
+                        Console.WriteLine(slownik[input]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie znam tego zwierzęcia");
+                    }
+                }
+                catch (Exception e) { Console.WriteLine(e.Message);
+            }
+
+        }
+        static Dictionary<string, string> UtworzSlownik(List<Dictionary<string,string>> list)
+        {
+            Dictionary<string, string> pustySlownik = new Dictionary<string, string>();
+            Dictionary<string,string> slownik = new Dictionary<string,string>();
+            for (int i = 0;i< list.Count; i++)
+            {
+                foreach (var j in list[i].Keys)
+                {
+                    if (j != string.Empty) slownik[j] = list[i][j];
+                    else return pustySlownik;
+                }
+                foreach (var j in list[i].Values)
+                {
+                    if (j == string.Empty) return pustySlownik;
+
+                }
+            }
+            return slownik;
+        }
+
+
+        static int[,] UtworzTablice(List<int> lista1, List<int> lista2, List<int> lista3)
+        {
+            int[,] tablica = new int[3,3];
+            if(lista1.Count == 3 && lista2.Count == 3 && lista3.Count == 3)
+            {
+                for(int i = 0; i < 3; i++)
+                {
+                    tablica[i,0] = lista1[i];
+                    tablica[i,1] = lista2[i];
+                    tablica[i,2] = lista3[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    tablica[i, 1] = 0;
+                    tablica[i, 2] = 0;
+                    tablica[i, 3] = 0;
+                }
+            }
+            return tablica;
+            
+        }
+
+        static List<int> UtworzListe(int n)
+        {
+            List<int> list = new List<int>();
+            if (n > 0)
+            {
+                for(int i = 0;i<n;i++)
+                {
+                    list.Add(0);
+                }
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+    }
+}
